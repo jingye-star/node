0e6e54cf39b7c7012117f5d0302a49250c906f94
zhangjingye <jingye.zhang@leyantech.com>
2021-04-28 14:37:33 +0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const http = require('http')
 
-const server = http.createServer()
+const server = http.createServer(app)
 
 const port = process.env.PROT || 3001
 server.listen(port, () => {
