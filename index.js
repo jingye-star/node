80feeac8e5edfa1a36fa1a5590cedd69bfcafeb7
zhangjingye <jingye.zhang@leyantech.com>
2021-04-28 14:36:10 +0800

    fix

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const url = `mongodb+srv://zhangjingye:NRpBtntU8j0gqLSf@cluster0.bn6hs.mongodb.net/cloud?retryWrites=true&w=majority`;
-const app = express();
-const port = 3001;
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(urlencodedParser);
+const app = require('./app')
+const http = require('http')
 
-app.use(express.static('build'))
+const server = http.createServer()
 
-app.use(jsonParser);
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-
-
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-const Note = mongoose.model("Note", noteSchema);
-app.get("/", async (req, res) => {
-  const notes = await Note.find({})
-  res.json(notes);
-});
-app.get("/v2", async (req, res) => {
-  const notes = await Note.find({})
-  res.json(notes);
-});
-app.post("/add", async (req, res) => {
-  const note = new Note(req.body);
-  await note.save().then((res) => {
-    console.log(res, "done");
-  });
-  res.send("done");
-});
-
-
-app.listen(3001, function () {
-  //在3001端口启动
-  console.log("Example app listening on port 3000");
-});
+const port = process.env.PROT || 3001
+server.listen(port, () => {
+    console.log('3001');
+})
